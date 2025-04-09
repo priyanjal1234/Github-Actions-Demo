34ba3bb7b4ee8864aa52398304cc6cb8180ac4d4
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-04-09 09:52:27 +0530

    Created the basic express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello to Github Actions")
+import express from 'express'
+const app = express()
+
+app.get("/",function(req,res) {
+    res.send("Hello From Express to Github Actions")
+})
+
+const port = 3000
+app.listen(port,function() {
+    console.log(`Server is running on port ${port}`)
+})
